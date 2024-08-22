a9792f14ad0ffd7916ca0ed1b09c87536f758c03
d-arevalo12 <diego.arevalo@improving.com>
2024-08-22 13:14:43 -0600

    Added roll button logic

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var diceOneImageView: UIImageView!
+  @IBOutlet weak var diceTwoImageView: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    diceOneImageView.image = UIImage(named: "DiceSix")
+    diceTwoImageView.image = UIImage(named: "DiceTwo")
+  }
+
+  @IBAction func rollButton(_ sender: UIButton) {
+    diceOneImageView.image = UIImage(named: getRandomDice())
+    diceTwoImageView.image = UIImage(named: getRandomDice())
+  }
+
+  func getRandomDice() -> String {
+    let randomInt: Int = Int.random(in: 1...6)
+    switch randomInt {
+      case 1: return "DiceOne"
+      case 2: return "DiceTwo"
+      case 3: return "DiceThree"
+      case 4: return "DiceFour"
+      case 5: return "DiceFive"
+      case 6: return "DiceSix"
+      default: return ""
     }
-
-
+  }
 }
 
